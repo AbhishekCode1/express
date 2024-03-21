49f0ce92531eba2f5477c3481e79cf8e69ee9c86
AbhishekCode1 <shivthakur166@gmail.com>
2024-03-21 10:09:56 +0530

    router in express

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const mainRouter = require('./routes/index')
 
 const app = express();
 
@@ -8,26 +9,8 @@ app.set('view engine','ejs'); // ejs template engine
 //console.log(app.get('view engine'));
 //console.log(app.get('views'));
 app.use(express.static('public')); // for static middleware
-
-app.get('/',(req,res)=>{
-   // res.send('hello from express')
-  // res.sendFile(path.resolve(__dirname)+'/index.html');
-  res.render('index',{
-   title:'My home page'
-  })
-});
-
-app.get('/about',(req,res)=>{
-    
-   // res.sendFile(path.resolve(__dirname)+'/about.html');
-    res.render('about',{
-      title:'My about page'
-    })
- })
- app.get('/download',(req,res)=>{
-    
-    res.download(path.resolve(__dirname)+'/about.html'); // for download
- })
+//app.use(mainRouter) // for router
+app.use('/en',mainRouter); //you can do it also 
 
 app.listen(PORT, ()=>{
     console.log(`listening on the port ${PORT}`);  //PORT=5000 node server.js 
